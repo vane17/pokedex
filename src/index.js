62df4fe9fff5fd7f9e4e7917d5e43dfdda183281
Vanessa AriasJ <ariarivane@gmail.com>
2022-09-16 11:28:06 -0500

    middlewares

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {pokemonsReducer} from './reducers/pokemons';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware,
+  compose,
+  legacy_createStore as createStore } from 'redux';
+import {featuring, logger} from './middlewares/index';
 import App from './App';
 import './index.css';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const composeEnharces = compose( window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+applyMiddleware(logger, featuring) ); //se pasa los enhancer 
+
+
 const store = createStore(
   pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnharces,
 );
 
 root.render(
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,1 +1,19 @@
 //cada vez que se dispare una accion se pueda obtener la informacion de esa accion y hacer consol log
+//funcion que retona otra funcion que
+
+
+export const logger = (store) => (next) => (action) => {
+    console.log(action);
+    next(action);
+}
+
+
+//agrega un pokemon que yo quiero //modifica pokemon personalizado
+export const featuring = (store) => (next) => (actionInfo) => {
+    const featured = [{ name: 'eddie' }, ...actionInfo.action.payload];
+    const updatedActionInfo = {
+      ...actionInfo,
+      action: { ...actionInfo.action, payload: featured },
+    };
+    next(updatedActionInfo);
+  };
